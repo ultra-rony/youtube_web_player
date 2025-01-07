df08cd5a88bd0f437b28e33f74ed5145958a1af6
Roman Diakonov <vegappunk@gmail.com>
2025-01-08 02:43:59 +0300

    fix: warning

--- lib/src/utils/extensions/map_response_convert_extension.dart
+++ lib/src/utils/extensions/map_response_convert_extension.dart
@@ -1,5 +1,5 @@
-/// An extension on Map<Object?, Object?> to provide a method for converting
-/// its entries to a Map<String, String>.
+// An extension on Map<Object?, Object?> to provide a method for converting
+// its entries to a Map<String, String>.
 extension MapResponseConvertExtension on Map<Object?, Object?> {
   /// Converts the map's entries to a new map with String keys and values.
   ///
@@ -7,7 +7,7 @@ extension MapResponseConvertExtension on Map<Object?, Object?> {
   /// a new map with string representations of the keys and values.
   ///
   /// Returns:
-  /// - A new Map<String, String> containing the converted entries.
+  // - A new Map<String, String> containing the converted entries.
   Map<String, String> toStringMap() {
     // Create an empty map to hold the state values.
     final stateMap = <String, String>{};
